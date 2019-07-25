e1fff603bbe2148cbc853a5661476f18d0d4e220
Watchful1 <watchful@watchful.gr>
2019-07-24 21:06:57 -0700

    Update url

--- src/globals.py
+++ src/globals.py
@@ -8,6 +8,6 @@ WEBHOOK = None
 
 OVER_GG_API = "https://api.over.gg/matches/upcoming"
 FLAIR_LIST = "http://rcompetitiveoverwatch.com/static/flairs.json"
-PREDICTION_URL = "https://lerhond.pl/predictor/owl-2019-3/"
+PREDICTION_URL = "https://lerhond.pl/predictor/owl-2019-4/"
 
 AUTHORIZED_USERS = ["Watchful1", "AkshonEsports"]
