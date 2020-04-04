1a4b3f5c18806f4601ef2c1c20c7c3c8207614cf
Watchful1 <watchful@watchful.gr>
2020-04-03 19:19:50 -0700

    Update mapping

--- src/mappings.py
+++ src/mappings.py
@@ -104,7 +104,7 @@ competitions = [
 		]
 	),
 	Competition(
-		name="Overwatch Contenders 2020: Korea",
+		name="Overwatch Contenders 2020 Season 1: Korea",
 		discord=[
 			DiscordNotification(
 				type=DiscordType.COW,
