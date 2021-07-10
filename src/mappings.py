398497accd5d4366b288bda44f80175f0ccccd7b
Watchful1 <watchful@watchful.gr>
2021-07-10 11:08:03 -0700

    Summer showdown spoilers

--- src/mappings.py
+++ src/mappings.py
@@ -46,7 +46,7 @@ competitions = [
 		post_minutes_ahead=60,
 		leave_thread_minutes=4 * 60,
 		event_build_hours_ahead=96,
-		spoiler_stages=["June Joust: West", "June Joust: East", "June Joust: Tournament"]
+		spoiler_stages=["Summer Showdown: West", "Summer Showdown: East", "Summer Showdown: Tournament"]
 	),
 	Competition(
 		name="Overwatch Contenders 2021 Season 1: Australia",
